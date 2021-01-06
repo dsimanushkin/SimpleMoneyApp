0f64666bbeb333dba38d7e24feda23c85f2cfec1
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-06 09:02:31 -0500

    Added fragments and layouts

---
+++ app/src/main/java/com/devlab74/simplemoneyapp/ConfirmationFragment.kt
@@ -0,0 +1,28 @@
+package com.devlab74.simplemoneyapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.devlab74.simplemoneyapp.databinding.FragmentConfirmationBinding
+
+class ConfirmationFragment : Fragment() {
+
+    private var _binding: FragmentConfirmationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConfirmationBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/devlab74/simplemoneyapp/MainActivity.kt
+++ app/src/main/java/com/devlab74/simplemoneyapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.devlab74.simplemoneyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devlab74.simplemoneyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
